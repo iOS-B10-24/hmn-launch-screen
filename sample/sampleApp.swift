54b22cefdffea23025a1dc404c40565062fd8dcd
Htet Myet <htetmyetx@gmail.com>
2024-10-05 13:01:18 +0700

    -homework-1-launch-screen

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Mac Tonic on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
